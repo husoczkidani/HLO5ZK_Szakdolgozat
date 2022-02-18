83a1dc4ac595d460264ee74438bfbaaf3fc19cb7
Husóczki Dániel <husoczkidaniel@gmail.com>
2022-02-18 20:25:11 +0100

    Generate buttons dynamically

--- MinesweeperWithSolver/MinesweeperWithSolver/ViewModels/GameBoardViewModel.cs
+++ MinesweeperWithSolver/MinesweeperWithSolver/ViewModels/GameBoardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MinesweeperWithSolver.Models;
+﻿using Caliburn.Micro;
+using MinesweeperWithSolver.Models;
 using MinesweeperWithSolver.State;
 using System;
 using System.Collections.Generic;
@@ -6,12 +7,34 @@ using System.Text;
 
 namespace MinesweeperWithSolver.ViewModels
 {
-    public class GameBoardViewModel : BaseViewModel
+    public class GameBoardViewModel : BaseViewModel 
     {
-        private readonly GameBoard _gameboard;
+        private int _width;
+        public int Width { 
+            get => _width;
+            set 
+            {
+                _width = value;
+                OnPropertyChanged(nameof(Width));
+            } 
+        }
+        private int _height;
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                _height = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+
+        public BindableCollection<Tile> GameBoardTiles { get; set; }
         public GameBoardViewModel(IRenavigator endScreenRenavigator, GameBoard gameBoard)
         {
-            _gameboard = gameBoard;
+            Width = gameBoard.Width * 32;
+            Height = gameBoard.Height * 32 + 100;
+            GameBoardTiles = new BindableCollection<Tile>(gameBoard.Tiles);
         }
     }
 }
